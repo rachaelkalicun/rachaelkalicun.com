0a8e24abee514b4ea374a8c03b21599535ee012f
Rachael <rhg108@gmail.com>
2024-11-19 14:04:12 -0700

    make name h1 on home page

--- src/_components/shared/navbar.rb
+++ src/_components/shared/navbar.rb
@@ -1,5 +1,5 @@
 class Shared::Navbar < Bridgetown::Component
   def initialize(metadata:, resource:, current_sprint_url:)
-    @metadata, @resource = metadata, @current_sprint_url = current_sprint_url
+    @metadata, @resource = resource, resource, @current_sprint_url = current_sprint_url
   end
 end
